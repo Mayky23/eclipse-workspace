11dbced252202e07e3de50d5600882959672f797
Miguel Ángel Roldán de Haro <mayky23dh@gmail.com>
2024-02-06 21:10:36 +0100

    6/02/2024
    - Correción servidor Accceso a datos Pelis

--- AccesoDatos/src/ServerPeliculas/Servidor.java
+++ AccesoDatos/src/ServerPeliculas/Servidor.java
@@ -4,64 +4,51 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
+
+
+
 
 public class Servidor {
 
-    private ServerSocket servidor;
-    private Socket cliente;
-    private ObjectInputStream entrada;
-    private MongoClient mongoClient;
-    private MongoDatabase database;
-    private MongoCollection<Document> collection;
+    private final ServerSocket servidor;
+    private final MongoCollection<Document> collection;
 
-    public Servidor() {
-        try {
-            servidor = new ServerSocket(27017);
-            System.out.println("Servidor esperando conexiones...");
-            mongoClient = new MongoClient("localhost", 27017);
-            database = mongoClient.getDatabase("cine");
-            collection = database.getCollection("peliculas");
-            while (true) {
-                cliente = servidor.accept();
-                System.out.println("Cliente conectado desde " + cliente.getInetAddress().getHostName());
-                manejarConexion(cliente);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+    public Servidor(MongoClient mongoClient, MongoDatabase database) throws IOException {
+        servidor = new ServerSocket(27017);
+        System.out.println("Servidor esperando conexiones...");
+        this.collection = database.getCollection("peliculas");
+        while (true) {
+            Socket cliente = servidor.accept();
+            System.out.println("Cliente conectado desde " + cliente.getInetAddress().getHostName());
+            manejarConexion(cliente);
         }
     }
 
     private void manejarConexion(Socket cliente) {
-        try {
-            entrada = new ObjectInputStream(cliente.getInputStream());
+        try (ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream())) {
             Pelicula pelicula = (Pelicula) entrada.readObject();
             System.out.println("Pelicula recibida: " + pelicula);
 
             // Insertar o actualizar la película en MongoDB
-            
             Document doc = new Document("nombre", pelicula.getNombre());
             Document found = collection.find(doc).first();
             if (found != null) {
                 int votos = found.getInteger("votos") + 1;
                 int notaTotal = found.getInteger("notaTotal") + pelicula.getNota();
-                double notaMedia = (double) notaTotal / votos; // Casting para evitar división de enteros
+                double notaMedia = (double) notaTotal / votos;
                 collection.updateOne(found, new Document("$set", new Document("notaMedia", notaMedia)
                         .append("votos", votos).append("notaTotal", notaTotal)));
             } else {
-                collection.insertOne(new Document("nombre", pelicula.getNombre()).append("anio", pelicula.getAnio())
-                        .append("duracion", pelicula.getDuracion()).append("notaMedia", pelicula.getNota())
-                        .append("votos", 1).append("notaTotal", pelicula.getNota()));
+                Document nuevaPelicula = new Document("nombre", pelicula.getNombre())
+                        .append("anio", pelicula.getAnio())
+                        .append("duracion", pelicula.getDuracion())
+                        .append("notaMedia", pelicula.getNota())
+                        .append("votos", 1)
+                        .append("notaTotal", pelicula.getNota());
+                collection.insertOne(nuevaPelicula);
             }
         } catch (IOException | ClassNotFoundException ex) {
             ex.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) {
-        new Servidor();
     }
 }
